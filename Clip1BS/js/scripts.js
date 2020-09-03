ee4d84b2bb5a418743a501bfa77728246d3c61da
Juan <juanfe224@protonmail.com>
2020-09-03 19:42:58 -0300

    assets

--- Clip1BS/js/scripts.js
+++ Clip1BS/js/scripts.js
@@ -61,3 +61,61 @@
   });
 
 })(jQuery); // End of use strict
+
+
+// Adivinanza
+var contadorR = 0;    
+var respuesta = 0;
+var respuestaCorrecta = 2004; 
+var intentosRestantes = 4;
+
+function contadorRestantes(){
+    var html = "";
+    intentosRestantes--;
+    console.log(intentosRestantes)
+    html+=`<h3>${intentosRestantes}</h3>`;
+    document.getElementById("intentosMostrar").innerHTML = html;
+
+    
+}
+function contadorDeIntentos(){
+    contadorRestantes();
+    contadorR++;
+    respuesta = parseInt(document.getElementById("respuestaObtenida").value) ;
+    if(respuesta === this.respuestaCorrecta){
+        var html ="";
+        document.getElementById("botonIntento").disabled="true";
+        document.getElementById("respuestaObtenida").disabled="true";
+        html+=`<h5>¡Felicitaciones, es correcto!</h5> <h5>`;
+        document.getElementById("pistasDar").innerHTML = html;
+
+
+    }
+    else{
+    this.darPistas();
+    }
+};
+
+function darPistas(){
+    var html ="";
+    if(this.contadorR === 1) {
+        html+=`<h5>Te quedan 3 intentos</h5>`;
+        document.getElementById("pistasDar").innerHTML = html;
+    }
+    else if(this.contadorR === 2) {
+        html+=`<h5>Ayuda:</h5> <h5>Primer pista  </h5><h5>Fue en el mismo año que las olimpiadas de Atenas</h5>`;
+        document.getElementById("pistasDar").innerHTML = html;
+    }
+    else if(this.contadorR === 3 ) {
+        html+=`<h5>Ayuda:</h5> <h5>Segunda pista  </h5><h5>Fue entre el 2000 y el 2005</h5>`;
+        document.getElementById("pistasDar").innerHTML = html;
+    }
+    else if(this.contadorR <= 4) {
+        document.getElementById("botonIntento").disabled="true";
+        document.getElementById("respuestaObtenida").disabled="true";
+        html+=`<h5> Perdiste, si quiere saber mas sobre la historia de Ubuntu, puede encontrarla <a href="http://www.hortalinux.org/ca/historia-ubuntu-2/">aqui</a> </h5>`;
+        document.getElementById("pistasDar").innerHTML = html;
+    };
+
+};
+
